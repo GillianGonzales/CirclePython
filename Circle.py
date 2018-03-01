389dc3ac43bf2ae336cbd2fc87012e07154775f5
GillianGonzales <36240556+GillianGonzales@users.noreply.github.com>
2018-03-01 15:53:02 -0500

    Initial commit

---
+++ Circle.py
@@ -0,0 +1,22 @@
+#
+# Created By Gillian Gonzales
+# Created On Feb 27 2018
+# 
+# This program will calculate the circumfrence and the area of a circle using the users input
+
+# Importing Text
+print ("Put any digit then enter to add another digit")
+
+# Importing Variables
+UserInput = input()
+PI = 3.14
+OtherInput = 2
+
+# Making Function
+C = input(float(OtherInput) * float(PI) * float(UserInput))
+print ("That is the Circumfrence")
+A = input(float(PI) * float(UserInput)**2)
+print ("That is the Area")
+	
+print ("")
+input("end program")
